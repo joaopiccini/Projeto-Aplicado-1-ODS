f0e2ad18e4337b31de056d6aa71548da00302e39
Joao Piccini - Casa <joaopedropiccini@gmail.com>
2022-12-10 14:55:39 -0300

    Ajustado para apresentação

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -2,6 +2,13 @@ var dataArray = [];
 var valoresArray = [];
 var valoresTotal = 0;
 
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
+
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo')
+})
+
 $(document).ready(function() {
 
   $.ajax({
